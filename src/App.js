531c7f682d5af8802266f43cc6a375e2167e3e98
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-09-03 00:12:56 +0700

    implement boostrap and apply to add new column button

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import AppBar from "components/AppBar/AppBar"
 import BoardBar from "components/BoardBar/BoardBar"
 import BoardContent from "components/BoardContent/BoardContent"
 import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
 import "./App.scss"
 
 function App() {
