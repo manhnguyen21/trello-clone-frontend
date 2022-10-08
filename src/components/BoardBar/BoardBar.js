57d6663038b13e350ea58f95f782b9418f345db5
Giang Thanh Dat <gthanhdatpro@gmail.com>
2022-10-08 11:04:03 +0700

    fix: optimize board bar css

--- src/components/BoardBar/BoardBar.js
+++ src/components/BoardBar/BoardBar.js
@@ -16,69 +16,67 @@ const boardNameInputStyle = { fontSize: 18, fontWeight: "bold", height: 32 }
 const BoardBar = () => {
   return (
     <nav className="navbar-board">
-      <div className="navbar-board-container">
-        <IconContext.Provider
-          value={{
-            className: "navbar-board-btn-item--icon-start",
-            size: ICON_SIZE,
-          }}
-        >
-          <div className="navbar-board-container--left">
-            <div className="navbar-board-btn-item board-name">
-              <GrowingInput style={boardNameInputStyle} />
-            </div>
-            <TrelloButton className="navbar-board-btn-item only-icon">
-              <TiStarOutline
-                size={2}
-                className="navbar-board-btn-item--icon-start star-icon"
-              />
-            </TrelloButton>
-            <span className="navbar-board-btn-divider" />
-            <TrelloButton className="navbar-board-btn-item">
-              <FiUsers />
-              Workspace visible
-            </TrelloButton>
-            {/* <TrelloButton className="navbar-board-btn-item btn-without-icon">
+      <IconContext.Provider
+        value={{
+          className: "navbar-board-btn-item--icon-start",
+          size: ICON_SIZE,
+        }}
+      >
+        <div className="navbar-board-container--left">
+          <div className="navbar-board-btn-item board-name">
+            <GrowingInput style={boardNameInputStyle} />
+          </div>
+          <TrelloButton className="navbar-board-btn-item only-icon">
+            <TiStarOutline
+              size={2}
+              className="navbar-board-btn-item--icon-start star-icon"
+            />
+          </TrelloButton>
+          <span className="navbar-board-btn-divider" />
+          <TrelloButton className="navbar-board-btn-item">
+            <FiUsers />
+            Workspace visible
+          </TrelloButton>
+          {/* <TrelloButton className="navbar-board-btn-item btn-without-icon">
               Trello Workspace
             </TrelloButton> */}
-            <span className="navbar-board-btn-divider" />
-            <TrelloButton className="navbar-board-btn-item">
-              <HiViewBoards />
-              Board
-              <FiChevronDown className="navbar-board-btn-item--icon-end" />
-            </TrelloButton>
-          </div>
-          <div className="navbar-board-container--right">
-            <TrelloButton className="navbar-board-btn-item">
-              <AiFillThunderbolt />
-              Automation
-            </TrelloButton>
-            <TrelloButton className="navbar-board-btn-item">
-              <BiRocket />
-              Power-Ups
-            </TrelloButton>
-            <span className="navbar-board-btn-divider" />
-            <TrelloButton className="navbar-board-btn-item">
-              <IoFilterSharp />
-              Filter
-            </TrelloButton>
-            <span className="navbar-board-btn-divider" />
-            <img
-              className="user-avatar"
-              src={require("assets/images/user-avatar.png")}
-            />
-            <TrelloButton className="navbar-board-btn-item btn-share">
-              <AiOutlineUserAdd />
-              Share
-            </TrelloButton>
-            <span className="navbar-board-btn-divider" />
-            <TrelloButton className="navbar-board-btn-item  only-icon">
-              <BsThreeDots />
-              {/* Show Menu */}
-            </TrelloButton>
-          </div>
-        </IconContext.Provider>
-      </div>
+          <span className="navbar-board-btn-divider" />
+          <TrelloButton className="navbar-board-btn-item">
+            <HiViewBoards />
+            Board
+            <FiChevronDown className="navbar-board-btn-item--icon-end" />
+          </TrelloButton>
+        </div>
+        <div className="navbar-board-container--right">
+          <TrelloButton className="navbar-board-btn-item">
+            <AiFillThunderbolt />
+            Automation
+          </TrelloButton>
+          <TrelloButton className="navbar-board-btn-item">
+            <BiRocket />
+            Power-Ups
+          </TrelloButton>
+          <span className="navbar-board-btn-divider" />
+          <TrelloButton className="navbar-board-btn-item">
+            <IoFilterSharp />
+            Filter
+          </TrelloButton>
+          <span className="navbar-board-btn-divider" />
+          <img
+            className="user-avatar"
+            src={require("assets/images/user-avatar.png")}
+          />
+          <TrelloButton className="navbar-board-btn-item btn-share">
+            <AiOutlineUserAdd />
+            Share
+          </TrelloButton>
+          <span className="navbar-board-btn-divider" />
+          <TrelloButton className="navbar-board-btn-item  only-icon">
+            <BsThreeDots />
+            {/* Show Menu */}
+          </TrelloButton>
+        </div>
+      </IconContext.Provider>
     </nav>
   )
 }
