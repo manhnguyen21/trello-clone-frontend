b9fce5d7ac840f9d3e7d6987b877c9f387da5831
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-10-19 22:39:38 +0700

    feat: add style for card-detail-data (members and labels)

--- src/components/Card/CardDetailData/CardDetailData.js
+++ src/components/Card/CardDetailData/CardDetailData.js
@@ -1,5 +1,39 @@
-const CardDetailData = ({ className }) => {
-  return <div className={className ?? ""}>card detail data</div>
+import { AiOutlinePlus } from "react-icons/ai"
+import "./CardDetailData.scss"
+
+const CardDetailData = () => {
+  return (
+    <div className={`modal-card-detail-container `}>
+      <div className="card-detail-data">
+        <div className="card-detail-data__item">
+          <h3 className="card-detail-data__item-header">Members</h3>
+          <div className="members">
+            <img
+              className="members__item"
+              src={require("assets/images/user-avatar.png")}
+            />
+            <a href="#" className="members__item members__item--add">
+              <AiOutlinePlus size={16} />
+            </a>
+          </div>
+        </div>
+        <div className="card-detail-data__item">
+          <h3 className="card-detail-data__item-header">Labels</h3>
+          <div className="labels">
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--primary" />
+            <div className="labels__item labels__item--add">
+              <AiOutlinePlus size={16} />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default CardDetailData
