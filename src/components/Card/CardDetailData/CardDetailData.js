7daa274b917214fd552386365aeed79082c04212
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-10-20 22:16:37 +0700

    chore: add some users

--- src/components/Card/CardDetailData/CardDetailData.js
+++ src/components/Card/CardDetailData/CardDetailData.js
@@ -2,6 +2,13 @@ import Label from "components/Label/Label"
 import { AiOutlinePlus } from "react-icons/ai"
 import "./CardDetailData.scss"
 
+const members = [
+  "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png",
+  "https://isrdc.iitb.ac.in/images/man.png",
+  "https://w7.pngwing.com/pngs/312/283/png-transparent-man-s-face-avatar-computer-icons-user-profile-business-user-avatar-blue-face-heroes-thumbnail.png",
+  "https://cdn1.iconfinder.com/data/icons/user-pictures/100/female1-512.png",
+]
+
 const CardDetailData = () => {
   return (
     <div className={`modal-card-detail-container `}>
@@ -9,10 +16,13 @@ const CardDetailData = () => {
         <div className="card-detail-data__item">
           <h3 className="card-detail-data__item-header">Members</h3>
           <div className="members">
-            <img
+            {/* <img
               className="members__item"
               src={require("assets/images/user-avatar.png")}
-            />
+            /> */}
+            {members.map((src) => (
+              <img key={src} className="members__item" src={src} />
+            ))}
             <a href="#" className="members__item members__item--add">
               <AiOutlinePlus size={16} />
             </a>
@@ -25,8 +35,8 @@ const CardDetailData = () => {
             <Label color="yellow" />
             <Label color="sky" />
             <Label color="purple" />
-            <Label color="red" />
-            <Label color="blue" />
+            {/* <Label color="red" />
+            <Label color="blue" /> */}
             <div className="labels__item labels__item--add">
               <AiOutlinePlus size={16} />
             </div>
