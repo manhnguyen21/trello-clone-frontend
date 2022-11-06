2b99c79d2e41dabf5b9e5470b7edfb2c18fd312b
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-11-06 11:21:28 +0700

    fix: close drop content when clicked close button

--- src/components/TrelloDropdown/TrelloDropdown.js
+++ src/components/TrelloDropdown/TrelloDropdown.js
@@ -1,7 +1,11 @@
 import TrelloButton from "components/TrelloButton/TrelloButton"
+import { IconContext } from "react-icons"
+import { IoCloseOutline } from "react-icons/io5"
 import "./TrelloDropdown.scss"
 
 const TrelloDropdown = ({ children, header, dropDownContent }) => {
+  const handleClose = () => document.activeElement.blur()
+
   return (
     <div className="trello-dropdown" tabIndex={-1}>
       {children}
@@ -9,7 +13,11 @@ const TrelloDropdown = ({ children, header, dropDownContent }) => {
         <div className="trello-dropdown-container__header">
           <span></span>
           <h2>{header}</h2>
-          <TrelloButton type="transparent-darker">X</TrelloButton>
+          <TrelloButton type="transparent-brighter" onClick={handleClose}>
+            <IconContext.Provider value={{ size: "1.25rem" }}>
+              <IoCloseOutline />
+            </IconContext.Provider>
+          </TrelloButton>
         </div>
         <div className="trello-dropdown-container__content">
           {dropDownContent}
