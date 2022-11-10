e413144cee64e3ee2ff6b8365419c377d7cda6ca
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-11-10 22:17:27 +0700

    feat: add some content to create buttons

--- src/components/CreateButton/CreateButton.js
+++ src/components/CreateButton/CreateButton.js
@@ -1,23 +1,19 @@
 import TrelloButton from "components/TrelloButton/TrelloButton"
 import { IconContext } from "react-icons"
-import { TbLayoutBoardSplit } from "react-icons/tb"
 import "./CreateButton.scss"
 
-const CreateButton = () => {
+const CreateButton = ({ icon, title, content }) => {
   return (
     <TrelloButton className="create-container" type="transparent-darker">
       <div className="create-container__header">
         <div className="create-container__icon">
           <IconContext.Provider value={{ size: 20 }}>
-            <TbLayoutBoardSplit />
+            {icon}
           </IconContext.Provider>
         </div>
-        <div className="create-container__title">Create board</div>
+        <div className="create-container__title">{title}</div>
       </div>
-      <p className="create-container__content">
-        A board is made up of cards ordered on lists. Use it to manage projects,
-        track information, or organize anything.
-      </p>
+      <p className="create-container__content">{content}</p>
     </TrelloButton>
   )
 }
