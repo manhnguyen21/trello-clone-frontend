5cf2b8c0ef66bf43345ee85e8af8345a87c984d6
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-11-27 17:09:54 +0700

    feat: add labels

--- src/components/Filter/Filter.js
+++ src/components/Filter/Filter.js
@@ -1,12 +1,86 @@
 import Avatar from "components/Avatar/Avatar"
+import Label from "components/Label/Label"
 import TrelloCheckBox from "components/TrelloCheckBox/TrelloCheckBox"
 import { IconContext } from "react-icons"
 import { AiOutlineUser } from "react-icons/ai"
+import { FiClock } from "react-icons/fi"
+import { RiCalendarTodoFill } from "react-icons/ri"
 import "./Filter.scss"
+
+const filterGroups = [
+  {
+    groupName: "Member",
+    items: [
+      {
+        icon: <AiOutlineUser />,
+        text: "No member",
+      },
+      {
+        icon: (
+          <Avatar
+            src={
+              "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
+            }
+            alt={
+              "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
+            }
+          />
+        ),
+        text: "Card assign to me",
+      },
+    ],
+  },
+  {
+    groupName: "Due date",
+    items: [
+      {
+        icon: <RiCalendarTodoFill />,
+        text: "No dates",
+      },
+      {
+        icon: <FiClock />,
+        style: { backgroundColor: "rgb(235, 90, 70)", color: "white" },
+        text: "Overdue",
+      },
+      {
+        icon: <FiClock />,
+        style: { backgroundColor: "rgb(242, 214, 0)", color: "white" },
+        text: "Due in the next day",
+      },
+      {
+        icon: <FiClock />,
+        text: "Due in the next week",
+      },
+      {
+        icon: <FiClock />,
+        text: "Due in the next month",
+      },
+      {
+        text: "Marked as complete",
+      },
+      {
+        text: "Not marked as complete",
+      },
+    ],
+  },
+  {
+    groupName: "Labels",
+    items: [
+      {
+        text: <Label width="100%" />,
+      },
+      {
+        text: <Label width="100%" color="dark-blue" />,
+      },
+      {
+        text: <Label width="100%" color="purple" />,
+      },
+    ],
+  },
+]
 
 const FilterGroup = ({ label, onChange, items }) => {
   const handleChange = (e) => {
-    console.log("e", e)
     onChange(e.target.value)
   }
 
@@ -26,12 +100,24 @@ const FilterGroup = ({ label, onChange, items }) => {
       )}
       {items?.map((item) => {
         return (
-          <div className="filter-item" key={item.id}>
+          <div
+            className={`filter-item ${label === "Labels" ? "label" : ""}`}
+            key={item.id}
+          >
             <TrelloCheckBox onChange={handleChange} />
-            <IconContext.Provider value={{ size: 16 }}>
-              <span className="filter-item__icon">{item.icon}</span>
-            </IconContext.Provider>
-            <span>{item.text}</span>
+            <div className="filter-item-content">
+              {item.icon && (
+                <IconContext.Provider value={{ size: 16 }}>
+                  <span
+                    className="filter-item-content__icon"
+                    style={item.style}
+                  >
+                    {item.icon}
+                  </span>
+                </IconContext.Provider>
+              )}
+              <span className="filter-item-content__text">{item.text}</span>
+            </div>
           </div>
         )
       })}
@@ -42,36 +128,14 @@ const FilterGroup = ({ label, onChange, items }) => {
 const Filter = () => {
   return (
     <div className="filter-container">
-      <FilterGroup
-        label={"Keyword"}
-        onChange={(searchKeyWord) =>
-          console.log("searchKeyWord", searchKeyWord)
-        }
-        text
-      />
-      <FilterGroup
-        label={"Member"}
-        onChange={(checkedItems) => console.log("checkedItems", checkedItems)}
-        items={[
-          {
-            icon: <AiOutlineUser />,
-            text: "No member",
-          },
-          {
-            icon: (
-              <Avatar
-                src={
-                  "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
-                }
-                alt={
-                  "https://www.pngall.com/wp-content/uploads/5/Profile-Male-PNG.png"
-                }
-              />
-            ),
-            text: "Card assign to me",
-          },
-        ]}
-      />
+      <FilterGroup label={"Keyword"} text />
+      {filterGroups.map((group) => (
+        <FilterGroup
+          key={group.groupName}
+          label={group.groupName}
+          items={group.items}
+        />
+      ))}
     </div>
   )
 }
