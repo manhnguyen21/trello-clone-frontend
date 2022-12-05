f3a1461297bfa130bab2a1a4b2a5df464e13981f
Giang Thành Đạt <gthanhdatpro@gmail.com>
2022-12-05 22:12:30 +0700

    User local server =]]z

--- src/utilities/constants.js
+++ src/utilities/constants.js
@@ -1,4 +1,4 @@
-export const API_ENDPOINT = "https://trello-clone-gthanhdatpro.herokuapp.com"
+export const API_ENDPOINT = "http://localhost:8017"
 export const MODAL_ACTION_CLOSE = "MODAL_ACTION_CLOSE"
 export const MODAL_ACTION_CONFIRM = "MODAL_ACTION_CONFIRM"
 export const ICON_SIZE = 20
