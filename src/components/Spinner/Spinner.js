eaa04ab0da0c900b7aee7404637066761c43b223
Giang Thanh Dat <gthanhdatpro@gmail.com>
2023-01-14 11:59:00 +0700

    feat: add Spinner component

---
+++ src/components/Spinner/Spinner.js
@@ -0,0 +1,18 @@
+import "./Spinner.scss"
+const Spinner = () => {
+  return (
+    <>
+      <div className="trello-spinner">
+        <div className="trello-spinner">
+          <div className="trello-spinner">
+            <div className="trello-spinner">
+              <div className="trello-spinner"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default Spinner
